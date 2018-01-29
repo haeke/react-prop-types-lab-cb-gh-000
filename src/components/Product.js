dc21ebfb9f51e2cb220fdff0889d1829ef6f58a2
edwin rivera <waehake@gmail.com>
2018-01-29 16:15:42 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,3 +1,11 @@
 // Code Product Component Here
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+
+class Product extends Component {
+  render() {
+    return ();
+  }
+}
+
+export deafult Product;
